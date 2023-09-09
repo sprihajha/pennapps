1b40f1633a44f2510ab5183c0dbb163e23a6a4db
Spriha Jha <jha.spriha@gmail.com>
2023-09-09 04:03:27 -0400

    Setup src folder

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+import React, { useState } from "react";
+import CheckboxCard from "./components/CheckboxCard";
+import Cart from "./components/Cart";
+import CartButton from "./components/CardButton";
+import MetaphorSearch from "./components/MetaphorSearch";
+import results from "./searchResults.json";
+
+const App = () => {
+  const [selectedItems, setSelectedItems] = useState({});
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const handleAdd = (index, item) => {
+    setSelectedItems({
+      ...selectedItems,
+      [index]: item,
+    });
+  };
+
+  const handleRemove = (index) => {
+    const updatedItems = { ...selectedItems };
+    delete updatedItems[index];
+    setSelectedItems(updatedItems);
+  };
+
+  const selectedItemsArray = Object.values(selectedItems);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Shop</h1>
+        <CartButton
+          count={selectedItemsArray.length}
+          onClick={() => setIsCartOpen(!isCartOpen)}
+        />
+      </header>
+      <MetaphorSearch />
+      {results?.results?.map((result, index) => (
+        <CheckboxCard
+          key={index}
+          title={result.title}
+          url={result.url}
+          onAdd={() => handleAdd(index, result)}
+        />
+      ))}
+      <Cart
+        items={selectedItems}
+        open={isCartOpen}
+        onClose={() => setIsCartOpen(false)}
+        onRemove={handleRemove}
+      />
+    </div>
+  );
+};
+
+export default App;
