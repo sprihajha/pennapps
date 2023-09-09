e39e87b94661127e33a686a0f2d899ab3b49df5d
Andrea Kim <80376288+aecone@users.noreply.github.com>
2023-09-09 01:29:34 -0400

    Search Page Formatted

---
+++ my-app/src/components/ItineraryGenerator.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ItineraryGenerator() {
+  return <div></div>;
+}
--- my-app/src/components/MetaphorSearch.js
+++ my-app/src/components/MetaphorSearch.js
@@ -1,28 +1,57 @@
-
 import { useState } from "react";
-import Metaphor from "metaphor-node";
+// import Metaphor from "metaphor-node";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./metaSearch.css";
 
 export default function MetaphorSearch() {
-  const [input, setInput] = useState("");
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
 
-  const formSubmit = async (e) => {
-    try{ 
-      const response = await Metaphor.search({input})
-      console.log(response)
-    }catch(err){
-      console.error(err)
-    }
-  }
+  //   const formSubmit = async (e) => {
+  //     try{
+  //       const response = await Metaphor.search({input})
+  //       console.log(response)
+  //     }catch(err){
+  //       console.error(err)
+  //     }
+  //   }
   return (
-    <>
-      <input
-        id="trip-info"
-        label="What do you want to do?"
-        variant="outlined"
-        value={input}
-        onChange={(e)=>{setInput(e.target.value)}}
-      />
-      <button variant='outlined' onClick={()=>{formSubmit()}}>Make Itinerary</button>
-        </>
-        )
-  }
+    <div className="container">
+      <div className="text-response">
+        <h1>I want to go to </h1>
+        <TextField
+          id="standard-basic"
+          label="Location?"
+          variant="standard"
+          value={input1}
+          onChange={(e) => {
+            setInput1(e.target.value);
+          }}
+          className="TextInput1"
+        />{" "}
+        <h1>to do </h1>
+        <TextField
+          id="standard-basic"
+          label="Activity?"
+          variant="standard"
+          value={input2}
+          onChange={(e) => {
+            setInput2(e.target.value);
+          }}
+          className="TextInput2"
+        />
+      </div>
+
+      <Button
+        variant="contained"
+        color="success"
+        onClick={() => {
+          //   formSubmit();
+        }}
+      >
+        Enter
+      </Button>
+    </div>
+  );
+}
