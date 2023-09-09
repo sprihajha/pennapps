474da42fb1594db9bccbd38e7184fed030fea6c4
Eric <echeung525@gmail.com>
2023-09-09 00:39:22 -0400

    second commit

--- my-app/src/components/MetaphorSearch.js
+++ my-app/src/components/MetaphorSearch.js
@@ -3,12 +3,13 @@ import Button from '@mui/material/Button';
 import { useState } from "react";
 import Metaphor from "metaphor-node";
 
-export default function () {
+export default function MetaphorSearch() {
   const [input, setInput] = useState("");
 
   async function fetchData(){
     try{
         const response = await Metaphor.search({input})
+        console.log(response)
     }
     catch(error){
         console.error("Error fetching data: ", error)
@@ -19,9 +20,9 @@ export default function () {
       <TextField
         id="trip-info"
         label="What do you want to do?"
-        variant="Outline"
+        variant="outlined"
         value={input}
-        onChange={setInput(e.target.value)}
+        onChange={(e)=>{setInput(e.target.value)}}
       />
       <Button variant='outlined' onClick={()=>{fetchData()}}>Make Itinerary</Button>
         </>
