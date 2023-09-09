f032ff807a9f7fad424fbb7dc2235d86343fe107
Kevin Li <kevin@ist-3138.apng.wlan.private.upenn.edu>
2023-09-09 00:46:39 -0400

    slight changes to MetaphorSearch

--- my-app/src/components/MetaphorSearch.js
+++ my-app/src/components/MetaphorSearch.js
@@ -1,30 +1,28 @@
-import TextField from "@mui/material/TextField";
-import Button from '@mui/material/Button';
+
 import { useState } from "react";
 import Metaphor from "metaphor-node";
 
 export default function MetaphorSearch() {
   const [input, setInput] = useState("");
 
-  async function fetchData(){
-    try{
-        const response = await Metaphor.search({input})
-        console.log(response)
-    }
-    catch(error){
-        console.error("Error fetching data: ", error)
+  const formSubmit = async (e) => {
+    try{ 
+      const response = await Metaphor.search({input})
+      console.log(response)
+    }catch(err){
+      console.error(err)
     }
   }
   return (
     <>
-      <TextField
+      <input
         id="trip-info"
         label="What do you want to do?"
         variant="outlined"
         value={input}
         onChange={(e)=>{setInput(e.target.value)}}
       />
-      <Button variant='outlined' onClick={()=>{fetchData()}}>Make Itinerary</Button>
+      <button variant='outlined' onClick={()=>{formSubmit()}}>Make Itinerary</button>
         </>
         )
   }
