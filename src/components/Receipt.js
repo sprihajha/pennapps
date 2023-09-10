ca952f6c42d72e502be18b1a5a4bb3f65b893500
Andrea Kim <80376288+aecone@users.noreply.github.com>
2023-09-10 01:31:20 -0400

    Add Twilio Test

--- src/components/Receipt.js
+++ src/components/Receipt.js
@@ -1,44 +1,72 @@
-import { Container, Typography, Paper, Divider, Button } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Paper,
+  Divider,
+  Button,
+  TextField,
+} from "@mui/material";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import "../Search.css";
 import CircularProgress from "@mui/material/CircularProgress";
+import axios from "axios";
 
 const Receipt = ({ items, days }) => {
   const itemArray = Object.values(items);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [data, setData] = useState(null);
 
   console.log(itemArray);
-  const generateItinerary = async () => {
-		setIsLoading(true);
-		console.log(days);
-		//https://autotourist.ue.r.appspot.com/api/itinerary
-		try {
-			const response = await fetch("http://localhost:3001/api/itinerary", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					days: days,
-					cart: itemArray,
-				}),
-			});
 
-			if (response.ok) {
-				const data = await response.json();
-				console.log(data);
-				setData(data);
-			} else {
-				console.error("Error:", response.statusText);
-			}
-		} catch (err) {
-			console.error(err);
-		} finally {
-			setIsLoading(false); // Hide the preloader once data is set or if an error occurs
-		}
+  const sendSMS = async () => {
+    try {
+      const accountSid = process.env.TWILIO_ACCOUNT_SID;
+      const authToken = process.env.TWILIO_AUTH_TOKEN;
+      const client = require("twilio")(accountSid, authToken);
+
+      client.messages
+        .create({
+          body: { data },
+          from: "+18339760526",
+          to: `+${phoneNumber}`,
+        })
+        .then((message) => console.log(message.sid))
+        .done();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const generateItinerary = async () => {
+    setIsLoading(true);
+    console.log(days);
+    //https://autotourist.ue.r.appspot.com/api/itinerary
+    try {
+      const response = await fetch("http://localhost:3001/api/itinerary", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          days: days,
+          cart: itemArray,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setData(data);
+      } else {
+        console.error("Error:", response.statusText);
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false); // Hide the preloader once data is set or if an error occurs
+    }
   };
 
   return (
@@ -79,18 +107,40 @@ const Receipt = ({ items, days }) => {
           {isLoading ? (
             <CircularProgress sx={{ m: "2rem" }} />
           ) : data ? (
-            <Paper elevation={3} style={styles.boardingPass}>
-              <Typography variant="h4" gutterBottom style={styles.header}>
-                Generated Itinerary:
-              </Typography>
-              <div>
-                {data.split("\n").map((line, index) => (
-                  <Typography key={index} variant="body1">
-                    {line}
-                  </Typography>
-                ))}
-              </div>
-            </Paper>
+            <>
+              {" "}
+              <Paper elevation={3} style={styles.boardingPass}>
+                <Typography variant="h4" gutterBottom style={styles.header}>
+                  Generated Itinerary:
+                </Typography>
+                <div>
+                  {data.split("\n").map((line, index) => (
+                    <Typography key={index} variant="body1">
+                      {line}
+                    </Typography>
+                  ))}
+                </div>
+              </Paper>
+              <TextField
+                id="standard-basic"
+                label="Enter Phone Number"
+                variant="standard"
+                onChange={(e) => {
+                  setPhoneNumber(e.target.value);
+                }}
+              />
+              <Button
+                variant="contained"
+                onClick={sendSMS}
+                style={{
+                  fontFamily: "Inter",
+                  fontSize: "1rem",
+                  margin: "1rem 0 2rem 0",
+                }}
+              >
+                Share via SMS
+              </Button>
+            </>
           ) : null}
         </div>
       </motion.div>
